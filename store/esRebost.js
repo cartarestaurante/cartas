5367cfa8986141b446f4b7f57627187754fc774d
Javi Puche <javierpuchevictoria@gmail.com>
2020-05-09 20:24:37 +0200

    added params

---
+++ store/esRebost.js
@@ -0,0 +1,106 @@
+export const state = () => ({
+    name: 'Es Rebost',
+    logo: 'https://www.esrebost.com/themes/demo/assets/images/logofooter.svg',
+    currency: '€',
+    categories: [
+        {
+            name: 'Entrantes',
+            dishes: [
+                {
+                    name: 'Ensalada Mixta',
+                    ingredients: [
+                        {
+                            id: 1,
+                            name: 'Lechuga'
+                        },
+                        {
+                            id: 2,
+                            name: 'Queso'
+                        },
+                        {
+                            id: 2,
+                            name: 'Tomate'
+                        }
+                    ],
+                    price: [
+                        {
+                            label: 'Pequeña',
+                            value: 14.45
+                        },
+                        {
+                            label: 'Mediana',
+                            value: 24.45
+                        },
+                        {
+                            label: 'Grande',
+                            value: 34.45
+                        }
+                    ],
+                    image: 'https://mimenurealfooding.com/wp-content/uploads/ensalada-mixta.jpeg'
+                },
+                {
+                    name: 'Ensalada Cesar',
+                    ingredients: [
+                        {
+                            id: 1,
+                            name: 'Lechuga'
+                        },
+                        {
+                            id: 2,
+                            name: 'Queso'
+                        },
+                        {
+                            id: 2,
+                            name: 'Tomate'
+                        }
+                    ],
+                    price: 12.52,
+                    image: 'https://s3.amazonaws.com/menuperu.pe/media/receta/Ensalada%20mixta-1638.jpeg'
+                }
+            ]
+        },
+        {
+            name: 'Carnes',
+            dishes: [
+                {
+                    name: 'Pollo a la plancha',
+                    ingredients: [
+                        {
+                            id: 1,
+                            name: 'Lechuga'
+                        },
+                        {
+                            id: 2,
+                            name: 'Queso'
+                        },
+                        {
+                            id: 2,
+                            name: 'Tomate'
+                        }
+                    ],
+                    price: 24.52,
+                    image: 'https://mimenurealfooding.com/wp-content/uploads/ensalada-mixta.jpeg'
+                },
+                {
+                    name: 'Ternera al horno',
+                    ingredients: [
+                        {
+                            id: 1,
+                            name: 'Lechuga'
+                        },
+                        {
+                            id: 2,
+                            name: 'Queso'
+                        },
+                        {
+                            id: 2,
+                            name: 'Tomate'
+                        }
+                    ],
+                    price: 12.52,
+                    image: 'https://s3.amazonaws.com/menuperu.pe/media/receta/Ensalada%20mixta-1638.jpeg'
+                }
+            ]
+        }
+    ]
+})
